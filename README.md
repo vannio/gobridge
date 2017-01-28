d3fdf808252e945dbb889ecc0e2131cf44989e5c
Van <singularity@vannio.co.uk>
2017-01-28 16:30:52 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GoBridge Session 1 Notes
+
+Some notes taken from session 1 of GoBridge London (28/Jan/2017), for those who wiped all their progress in the go playground :anguished:
--- notes/floats.go
+++ notes/floats.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+  // Floats are numbers with decimal places
   // Only use floats when necessary
   // Uses more memory(?) than integers
   exampleFloat := 0.5
