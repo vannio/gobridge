82a57239c8c3a45ab9894c077df3dcaffa482aef
Van <singularity@vannio.co.uk>
2017-02-11 11:03:09 +0000

    Convert bytes back into go object

--- week-3/client/main.go
+++ week-3/client/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+// Pet : a subset of animals
+type Pet struct {
+	Name    string
+	Hobbies []string
+	Likes   int
+}
 
 func main() {
 	res, err := http.Get("http://localhost:9000/list")
@@ -27,5 +35,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println(string(data))
+	// We want to read JSON and save it
+	// Source data is an array/slice of pets
+	pets := []Pet{}
+
+	// We want Unmarshal to modify pet so we pass in a reference
+	err = json.Unmarshal(data, &pets)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// json.Unmarshal needs a slice of bytes (data) and a 'schema' in form of a blank interface
+	// interface can be anything(?) - lowest form of object
+	// Could be a value, could be a reference
+	log.Println(pets)
 }
